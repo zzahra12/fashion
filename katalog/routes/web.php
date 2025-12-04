cc5b1c8051cfb23652a8aed7dd2498a7ed665a9b
itsmemozha <megantaramozhapratama@gmail.com>
2025-12-04 16:50:11 +0700

    baru

--- katalog/routes/web.php
+++ katalog/routes/web.php
@@ -2,22 +2,24 @@
 $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
+$view = null; // default
+
 if ($page == 'genre') {
 
     if ($action == 'create') {
-        include '../page/admin/admin-page/input_genre.php';
+        $view = '../page/admin/admin-page/input_genre.php';
     }
 
     elseif ($action == 'store') {
-        include '../page/admin/admin-page/store_genre.php';
+        $view = '../page/admin/admin-page/store_genre.php';
     }
 
     elseif ($action == 'index') {
-        include '../page/admin/admin-page/list_genre.php';
+        $view = '../page/admin/admin-page/list_genre.php';
     }
 
     else {
-        echo "<h3>Halaman Tidak Ditemukan</h3>";
+        $view = null;
     }
 }
 ?>
